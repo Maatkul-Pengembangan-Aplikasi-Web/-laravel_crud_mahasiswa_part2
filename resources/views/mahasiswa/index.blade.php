686bd31c9e2a19ed58b23311c23d3cd70277e5c9
aaherdi <aaherdi832@gmail.com>
2024-11-28 11:01:58 +0700

    update readme

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,36 +1,3 @@
-### Setup Migration + Model + Controller untuk Mahasiswa
-1. pada terminal masukan command `php artisan make:model Mahasiswa -mc -r` untuk menggenerate migration & model tabel mahasiswa dan MahasiswaController beserta resource nya
-2. buka file migration mahasiswa pada `database\migrations\xxxx_create_mahasiswas_table.php` masukan query berikut untuk menambahkan isi tabel mahasiswas ketika dilakuakn migration
-```
-   public function up(): void
-    {
-        Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama');
-            $table->integer('npm');
-            $table->string('prodi');
-            $table->string('foto')->nullable();
-            $table->timestamps();
-        });
-    }
-```
-3. pada file model Mahasiswa.php masukan query berikut
-```
-    use HasFactory;
-    protected $table = 'mahasiswas';
-    protected $fillable = [
-        'nama',
-        'npm',
-        'prodi',
-        'foto',
-    ];
-```
-5. lakukan migration untuk menambahkan tabel mahasiswas pada database dengan command `php artisan migrate`
-
-=================
-### Setup Halaman Mahasiswa
-1. buat folder mahasiswa dan file index.blade.php pada folder views, masukan kode berikut untuk tampilan halaman mahasiswa
-```
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -87,21 +54,3 @@
         </div>
     </div>
 </x-app-layout>
-```
-2. pada file `ProdiController` masukan kode untuk function index halaman mahasiswa
-```
-    public function index()
-    {
-        return view('mahasiswa.index');
-    }
-```
-3. buat route ke halaman mahasiswa `web.php`
-```
-Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('/mahasiswa');
-```
-4. agar halaman mahasiswa masuk ke menu navigasi tambahkan pada file `navigation.blade.php`, tambahkan kode berikut pada bagian navigation links
-```
-<x-nav-link :href="route('/mahasiswa')" :active="request()->routeIs('/mahasiswa')">
-{{ __('Mahasiswa') }}
-</x-nav-link>
-```
